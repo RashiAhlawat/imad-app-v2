ec9884ded47fc36c7dde3ce395256e9d57c60377
RashiAhlawat <rashi.ahlawat89@gmail.com>
2017-02-09 20:24:39 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one' , function(req, res){
-    res.sendFile("Article one is requested and will be displayed");
+    res.sendFile('Article one is requested and will be displayed');
 });
 
 app.get('/article-two',function(req, res){
-    res.sendFile("Article two is requested and will be displayed");
+    res.sendFile('Article two is requested and will be displayed');
 });
 
 app.get('/article-three',function(req, res){
-    res.sendFile("Article three is requested and will be displayed");
+    res.sendFile('Article three is requested and will be displayed');
 });
 
 app.get('/ui/style.css', function (req, res) {
