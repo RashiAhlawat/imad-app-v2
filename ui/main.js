d55a01e65420400abcecf393b3fe8b0da824652e
RashiAhlawat <rashi.ahlawat89@gmail.com>
2017-02-13 15:41:20 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,19 +2,19 @@ console.log('Loaded!');
 
 //change the value of maintext
 
-var element = document.getElementById("main_text");
-element.innerHTML = "new value";
+// var element = document.getElementById("main_text");
+// element.innerHTML = "new value";
 
-var img = document.getElementById("madi");
-var marginPixel=0;
-function moveRight(){
-    marginPixel = marginPixel + 1;
-    img.style.marginLeft = marginPixel +"px";
-    img.style.marginTop = marginPixel +"px";
-}
-img.onclick = function(){
-    var intervL = setInterval(moveRight,50);
-};
+// var img = document.getElementById("madi");
+// var marginPixel=0;
+// function moveRight(){
+//     marginPixel = marginPixel + 1;
+//     img.style.marginLeft = marginPixel +"px";
+//     img.style.marginTop = marginPixel +"px";
+// }
+// img.onclick = function(){
+//     var intervL = setInterval(moveRight,50);
+// };
 
 var button = document.getElementById("counter");
 var counter =0;
