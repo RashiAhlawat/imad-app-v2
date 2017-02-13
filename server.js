79c0f82863a2853ff0a88c25907dabec7c4533ed
RashiAhlawat <rashi.ahlawat89@gmail.com>
2017-02-13 16:52:43 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var nameLists = [];
+app.get('/submit-name' , function(req ,res ){
+   var responseName = req.query.name;
+   var name = responseName.value;
+   nameLists.push(name);
+   res.send(JSON.stringify(nameLists));
+   
+});
+
 var counter =0;
 app.get('/counter', function (req, res) {
     counter = counter +1;
