e791731d32b8494d30e1493e51eaf737f0d37c3b
RashiAhlawat <rashi.ahlawat89@gmail.com>
2017-02-13 17:17:37 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ button.onclick = function(){
       {
           if(request.status === 200)
           {
-             var names = request.reresponseText ;
+             var names = request.responseText ;
              names = JSON.parse(names);
              var list ="";
              for(var i=0; i< names.length ;i++)
