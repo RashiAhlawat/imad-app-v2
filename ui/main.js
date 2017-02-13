96f700bb2d7afb7a98ef9b460b50515abc6bcfaa
RashiAhlawat <rashi.ahlawat89@gmail.com>
2017-02-13 16:04:08 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,11 +19,27 @@
 var button = document.getElementById("counter");
 var counter =0;
 button.onclick = function (){
-    // var request = XMLHttpRequest();
+ 
+ //Create request   
+  var request = XMLHttpRequest();
+  
+  //Action to be performed after request is made
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          if(request.status === 200)
+          {
+              var counter = request.responseText;
+              var span = document.getElementById("count");
+              span.innerHTML = counter.toString();
+          }
+      }
+      
+  };
+  
+  // Make a request
+  request.open("GET","http://rashiahlawat.imad.hasura-app.io/counter", true);
+  request.send(null);
     
-    
-counter = counter + 1;
-var span = document.getElementById("count");
-span.innerHTML = counter.toString();
      
 };
