db1f2c95dbd7cf1ac38707473f9e654dbf673379
RashiAhlawat <rashi.ahlawat89@gmail.com>
2017-02-13 17:15:14 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,20 +15,38 @@
 // img.onclick = function(){
 //     var intervL = setInterval(moveRight,50);
 // };
-var inputName = document.getElementById("name");
-var Name = inputName.value;
 var button = document.getElementById("submit_but");
 button.onclick = function(){
     
- var names = ["name1","name2","name3","name4"];   
- var list ="";
- for(var i=0; i< names.length ;i++)
- {
-     list += "<li>" +names[i] +"</li>";
- }
- var span = document.getElementById("nameList");
- span.innerHTML = list;
-    
+    var request = new XMLHttpRequest();
+  
+  //Action to be performed after request is made
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          if(request.status === 200)
+          {
+             var names = request.reresponseText ;
+             names = JSON.parse(names);
+             var list ="";
+             for(var i=0; i< names.length ;i++)
+             {
+                 list += "<li>" +names[i] +"</li>";
+             }
+             var span = document.getElementById("nameList");
+             span.innerHTML = list;
+          }
+      }
+      
+  };
+  
+  var inputName = document.getElementById("name");
+  var Name = inputName.value;
+  
+  // Make a request
+  request.open("GET","http://rashiahlawat.imad.hasura-app.io/submit-name?name=" + name, true);
+  request.send(null);
+ 
 };
 
 
